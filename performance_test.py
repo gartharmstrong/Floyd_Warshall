ab28a217324d3041be4ccf7023394c314e131886
sggarmst <sggarmst@liverpool.ac.uk>
2022-05-02 10:25:52 +0800

    Performance test imperative and recursive algo

--- performance_test.py
+++ performance_test.py
@@ -1,11 +1,37 @@
 from recursive_floyd import recursivepaths
-import imperative_floyd
+from imperative_floyd import imperativepaths
 import datetime
 import timeit
 
+"""Performance tests using both brute force and timeit library
+on both the imperative and recusrive implementation of Floyd's
+algorithm.
+"""
+
+# Run performance tests on recursion using internal datetime
+print("Recursion timing using brute force.")
 start_time = datetime.datetime.now()
 recursivepaths()
 end_time = datetime.datetime.now()
-print(end_time - start_time)
+print("Recursion brute force timing: " + str(end_time - start_time))
 
-print(timeit.timeit(setup="from recursive_floyd import recursivepaths",stmt="recursivepaths()", number=10))
+# Run performance tests on imperative using internal datetime
+print("\n\nImperative timing using brute force.")
+start_time = datetime.datetime.now()
+imperativepaths()
+end_time = datetime.datetime.now()
+print("Imperative brute force timing: " + str(end_time - start_time))
+
+# Run performance tests on recursion using timeit
+print("\n\nRecusion timing using timeit library.")
+print("Recursion timeit timing: " + str(timeit.timeit(
+    setup="from recursive_floyd import recursivepaths",
+    stmt="recursivepaths()",
+    number=10)))
+
+# Run performance tests on imperative using timeit
+print("\n\nImperative timing using timeit library.")
+print("Imperative timeit timing: " + str(timeit.timeit(
+    setup="from imperative_floyd import imperativepaths",
+    stmt="imperativepaths()",
+    number=10)))
